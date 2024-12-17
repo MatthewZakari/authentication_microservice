9d08aab6b356c63dd88127188cd0403344acd09d
Matthew Zakari <zakarimatthew62@gmail.com>
2024-12-17 07:04:05 -0800

    configuration settings

---
+++ config/settings.py
@@ -0,0 +1,7 @@
+SECRET_KEY = "your_secret_key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+DB_HOST = "localhost"
+DB_NAME = "auth_service_db"
+DB_USER = "auth_user"
+DB_PASSWORD = "auth_password"
